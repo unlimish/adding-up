070d7573c18617d52cf46e1334e24e01320a2ee7
unlimish <mail@unlimish.xyz>
2020-07-14 22:08:58 +0900

    update rl.on(): data split

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ const readline = require('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({input: rs, output: {} });
 rl.on('line', lineString => {
-    console.log(lineString);
+    const  columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = columns[1];
+    const popu = parseInt(columns[3]);
 });
