60445300c39ee2900456660bd7afdaaa75f410ab
unlimish <mail@unlimish.xyz>
2020-07-15 15:36:39 +0900

    Refactor: by associative array and instancing

--- app.js
+++ app.js
@@ -3,13 +3,30 @@ const fs = require('fs'); // File System
 const readline = require('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({input: rs, output: {} });
+const prefectureDataMap = new Map(); // key: Prefecture value: objects of the sum total
 rl.on('line', lineString => {
     const  columns = lineString.split(',');
     const year = parseInt(columns[0]);
     const prefecture = columns[1];
     const popu = parseInt(columns[3]);
-
-    console.log(year);
-    console.log(prefecture);
-    console.log(popu);
+    if (year === 2010 || year === 2015) {
+        let value = prefectureDataMap.get(prefecture);
+        if (!value) {
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+        if (year === 2010) {
+            value.popu10 = popu;
+        }
+        if (year === 2015) {
+            value.popu15 = popu;
+        }
+        prefectureDataMap.set(prefecture, value)
+    }
+});
+rl.on('clone', () => {
+    console.log(prefectureDataMap);
 });
